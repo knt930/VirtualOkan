e50c8553ab94b2dbce6ed95e7bd11e07fdaef60d
knt930 <sword11wg@gmail.com>
2019-03-23 23:43:46 +0900

    初期設定

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,16 @@ module Zubora
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+    config.i18n.available_locales = [:ja, :en]
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.generators do |g|
+      g.javascripts false
+      g.helper false
+      g.test_framework false
+      g.decorator false
+    end
   end
 end
